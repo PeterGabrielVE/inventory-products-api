5ab345cf85611414630d4d00d55800bbeffb3311
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-05 10:41:08 -0400

    category && product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,9 @@ class Category extends Model
 
     protected $table = 'categories';
     protected $fillable = ['name','description'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
